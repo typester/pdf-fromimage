1437b3070cf72eb0f703924427324fc9cf2e5b05
Daisuke Murase <typester@cpan.org>
2008-05-29 17:00:46 +0900

    Checking in changes prior to tagging of version 0.000001.  Changelog diff is:

--- lib/PDF/FromImage/Image.pm
+++ lib/PDF/FromImage/Image.pm
@@ -19,5 +19,24 @@ has height => (
     required => 1,
 );
 
+=head1 NAME
+
+PDF::FromImage::Image - PDF::FromImage image object
+
+=head1 AUTHOR
+
+Daisuke Murase <typester@cpan.org>
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
+
+
 1;
 
